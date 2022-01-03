245620cb1b4274ee57c9e579f2af9824958641a4
AlexFreik <freik.aa@phystech.edu>
2022-01-03 18:31:31 +0100

    update name

--- RUTeam/setName.sh
+++ RUTeam/setName.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-rm ${1-'RUGuide.pdf'}
-cp main.pdf ${1-'RUGuide.pdf'}
+rm ${1-'montage_guide_ru.pdf'}
+cp main.pdf ${1-'montage_guide_ru.pdf'}
+
+rm ${2-'montage_guide_en.pdf'}
+cp main.pdf ${2-'montage_guide_en.pdf'}
